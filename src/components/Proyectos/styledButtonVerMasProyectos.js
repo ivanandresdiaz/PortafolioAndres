a38ad2044701cfb5901a3282a48349f3a3664914
Andres Diaz <ivanandresdilox@gmail.com>
2021-06-20 17:22:11 -0500

    listo proyectos

--- src/components/Proyectos/styledButtonVerMasProyectos.js
+++ src/components/Proyectos/styledButtonVerMasProyectos.js
@@ -11,11 +11,14 @@ align-items:center;
 export const ButtonVerMas = styled.button`
 width:50%;
 max-width: 250px;
+background-color:transparent;
 padding:2rem 0;
-border-radius:5px;
 display: flex;
 justify-content:center;
 align-items:center;
 color: #0F0E17;
 border: 1px solid #0F0E17;
+&:hover {
+  background-color: #e8e8ee;
+}
 `;
